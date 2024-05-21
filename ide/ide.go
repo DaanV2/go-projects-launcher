92ab03463757c4c52ed278b20409ea9efc564d13
DaanV2 <daanverstraten@hotmail.com>
2024-05-21 20:14:38 +0200

    Adding intial code

--- ide/ide.go
+++ ide/ide.go
@@ -1,9 +1,61 @@
 package ide
 
+import (
+	"fmt"
+	"maps"
+)
 
-type IDE_ID int
+type (
+	IDE_ID string
+
+	IDE interface {
+		// OpenCommand returns the command to open the IDE
+		OpenCommand() string
+		// Name returns the name of the IDE
+		Name() string
+		// ID returns the ID of the IDE
+		ID() IDE_ID
+		// RecommendPatterns returns a list of repo patterns that the IDE is recommended for
+		RecommendPatterns() []string
+	}
+
+	ideConfig struct {
+		_OpenCommand string
+		_Name        string
+		_ID          IDE_ID
+		_Recommend   []string
+	}
+)
+
+func (i ideConfig) OpenCommand() string { return i._OpenCommand }
+func (i ideConfig) Name() string        { return i._Name }
+func (i ideConfig) ID() IDE_ID          { return i._ID }
+func (i ideConfig) RecommendPatterns() []string { return i._Recommend }
+
+var ides = map[IDE_ID]IDE{}
+
+func registerIDE(ide ...IDE) {
+	for _, i := range ide {
+		if _, ok := ides[i.ID()]; ok {
+			panic(fmt.Errorf("IDE %s already registered", i.Name()))
+		} else {
+			ides[i.ID()] = i
+		}
+	}
+}
+
+// GetIDE returns the IDE with the given ID, or false if it does not exist
+func GetIDE(id IDE_ID) (IDE, bool) {
+	i, ok := ides[id]
+	return i, ok
+}
+
+// GetIDEs returns a copy of the map of IDEs
+func GetIDEs() map[IDE_ID]IDE {
+	return maps.Clone(ides)
+}
 
 const (
-	VSCODE IDE_ID = iota
-	VSCODE_WLS IDE_ID
+	VSCODE     IDE_ID = "vscode"
+	VSCODE_WLS IDE_ID = "wsl-vscode"
 )
--- ide/vscode.go
+++ ide/vscode.go
@@ -1,1 +1,18 @@
 package ide
+
+func init() {
+	registerIDE(
+		ideConfig{
+			"code",
+			"Visual Studio Code",
+			VSCODE,
+			[]string{"*"},
+		},
+		ideConfig{
+			"wsl code .",
+			"Visual Studio Code (WSL)",
+			VSCODE_WLS,
+			[]string{"wsl.localhost"},
+		},
+	)
+}
