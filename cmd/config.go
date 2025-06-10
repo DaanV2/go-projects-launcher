36b2b5274e508f19883bdde49c2530bf5d11b401
Daan Verstraten <daanverstraten@hotmail.com>
2025-06-10 10:15:07 +0000

    adding in that config can be opened in the ide
    Signed-off-by: GitHub <noreply@github.com>

--- cmd/config.go
+++ cmd/config.go
@@ -1,7 +1,29 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"path/filepath"
+
+	"github.com/DaanV2/go-projects-launcher/pkg/config"
+	"github.com/DaanV2/go-projects-launcher/pkg/ide"
+	"github.com/DaanV2/go-projects-launcher/pkg/projects"
+	"github.com/charmbracelet/log"
+	"github.com/spf13/cobra"
+)
 
 func ConfigWorkload(cmd *cobra.Command, args []string) {
-	// TODO a huh form that allows editing of the config
+	fp := config.ConfigFilepath()
+	log.Info("Configuration file path", "path", fp)
+
+	dir := filepath.Dir(fp)
+
+	userIde, _ := cmd.Flags().GetString("ide")
+	pro := &projects.Project{
+		Name:   "Tool Config",
+		Folder: dir,
+	}
+
+	err := invokeIDE(ide.GetIDE(userIde), pro, config.GetConfig())
+	if err != nil {
+		log.Fatal("error while invoking IDE", "error", err)
+	}
 }
