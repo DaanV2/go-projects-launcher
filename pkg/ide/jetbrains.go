fb1649cf986bc82a470729ec5da979b7b45e1545
Daan Verstraten <daanverstraten@hotmail.com>
2026-01-08 15:38:27 +0100

    refactor: condensed jetbrain products

--- pkg/ide/jetbrains.go
+++ pkg/ide/jetbrains.go
@@ -1,11 +1,25 @@
 package ide
 
+import (
+	"os/exec"
+	"runtime"
+)
+
 const (
+	GOLAND   IDE_ID = "goland"
 	INTELLIJ IDE_ID = "intellij"
 )
 
 func init() {
 	registerIDE(
+		ideConfig{
+			func(folder string) *exec.Cmd {
+				return exec.Command("idea", ".")
+			},
+			"Goland - Jetbrains",
+			GOLAND,
+			[]string{},
+		},
 		ideConfig{
 			func(folder string) *exec.Cmd {
 				return exec.Command("idea", ".")
