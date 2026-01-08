01d0314c3d1edd8a7472dcb2254b1918cf1f9298
Daan Verstraten <daanverstraten@hotmail.com>
2026-01-08 15:42:31 +0100

    Feature/jetbtrains (#21)
    * feat: adding intellij and goland
    
    * feat: adding intellij and goland
    
    * refactor: condensed jetbrain products
    
    * fix: cleaning imports
    
    * chore: upgrading version

---
+++ pkg/ide/jetbrains.go
@@ -0,0 +1,31 @@
+package ide
+
+import (
+	"os/exec"
+)
+
+const (
+	GOLAND   IDE_ID = "goland"
+	INTELLIJ IDE_ID = "intellij"
+)
+
+func init() {
+	registerIDE(
+		ideConfig{
+			func(folder string) *exec.Cmd {
+				return exec.Command("idea", ".")
+			},
+			"Goland - Jetbrains",
+			GOLAND,
+			[]string{},
+		},
+		ideConfig{
+			func(folder string) *exec.Cmd {
+				return exec.Command("idea", ".")
+			},
+			"Intellij - Jetbrains",
+			INTELLIJ,
+			[]string{},
+		},
+	)
+}
